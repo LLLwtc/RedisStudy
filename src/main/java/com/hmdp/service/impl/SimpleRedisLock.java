44b43f68148b1e3fa2261f80c2c609edb6d72c50
wangyueying@pconline.com.cn <wangyueying@pconline.com.cn>
2023-06-27 11:01:38 +0800

    lua脚本解决拿锁比锁删锁的原子性动作，防止线程1误删线程2的锁

--- src/main/java/com/hmdp/service/impl/SimpleRedisLock.java
+++ src/main/java/com/hmdp/service/impl/SimpleRedisLock.java
@@ -1,8 +1,12 @@
 package com.hmdp.service.impl;
 
 import com.hmdp.service.ILock;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -11,6 +15,15 @@ public class SimpleRedisLock implements ILock {
     String name;
     private static final String KEYPREFIX="lock:";
     private static final String ID_PREFIX= UUID.randomUUID().toString()+"-";
+
+    //lua脚本
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT=new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate,String name) {
         this.stringRedisTemplate = stringRedisTemplate;
@@ -29,12 +42,17 @@ public class SimpleRedisLock implements ILock {
     @Override
     public void unlock() {
         //获取线程标识
-        String threadId = ID_PREFIX+Thread.currentThread().getId();
+        //String threadId = ID_PREFIX+Thread.currentThread().getId();
         //获取锁的值
-        String id = stringRedisTemplate.opsForValue().get(KEYPREFIX + name);
+        //String id = stringRedisTemplate.opsForValue().get(KEYPREFIX + name);
         //确定是否是自己的锁
-        if(threadId==id){
-            stringRedisTemplate.delete(KEYPREFIX+name);
-        }
+        //if(threadId==id){
+        //    stringRedisTemplate.delete(KEYPREFIX+name);
+        //}
+        //使用lua脚本将拿锁比锁删锁封装为原子性操作
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEYPREFIX + name),
+                ID_PREFIX+Thread.currentThread().getId());
     }
 }
